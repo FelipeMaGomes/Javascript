28b6fc3be4953c34d338d43ccda9cb47389b37b4
FelipeMaGomes <felipe.martins.gomes@outlook.com>
2024-02-10 14:14:13 -0300

    verificação de idade

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,23 @@
-alert('Olá')
+function carregar() {
+    var hd = window.document.getElementById('hd')
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        // DIA
+        img.src = 'manhã-redondo.png'
+        window.document.body.style.background = '#f6c681'
+    } else if (hora >= 12 && hora < 18) {
+        // TARDE
+        img.src = 'tarde-redondo.png'
+        window.document.body.style.background = '#e15e05'
+        hd.innerHTML = 'Hora da Tarde'
+    } else {
+        // NOITE
+        img.src = 'noite-redondo.png'
+        window.document.body.style.background = '#03234e'
+        hd.innerHTML = 'Hora da Noite'
+    }
+}
