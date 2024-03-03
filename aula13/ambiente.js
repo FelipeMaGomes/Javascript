120609cd13bdc2e77f5b21724dafdcefa024796d
FelipeMaGomes <felipe.martins.gomes@outlook.com>
2024-03-03 16:13:07 -0300

    while

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+do {
+    console.log(`Numero atual ${c}`)
+    c++
+} while (c <= 6) 
+
+
+/*
+while (c <= 6) {
+    console.log(`Numero atual ${c}`)
+    c++ // c = c + 1 
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
