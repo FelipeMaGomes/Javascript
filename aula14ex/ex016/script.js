74718f16a083dfe3fb66ae65e70f7e70c7efd480
FelipeMaGomes <felipe.martins.gomes@outlook.com>
2024-03-04 14:06:39 -0300

    exercicio

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,11 @@
+function contar() {
+    //var ini = getElementById('ini')
+    //var fim = getElementById('fim')
+    //var passo = getElementById('passo')
+   //var conta = getElementById('conta')
+    var res = getElementById('res')
+    res.innerHTML = 'Contando: '
+    for (var c = 1; c <= 8; c += 1) {
+        console.log(`${c} `)
+    }
+}
