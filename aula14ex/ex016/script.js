371df32633fbb509511ab92845e34cb3dfb401d2
FelipeMaGomes <felipe.martins.gomes@outlook.com>
2024-03-18 13:22:16 -0300

    contagem

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,13 +1,29 @@
 function contar() {
-    var i = document.getElementById("ini").value
-    var f = document.getElementById("fim").value 
-    var p = document.getElementById("passo").value
-    var r = document.getElementById("res") 
-    r.innerHTML = 'Contando: ';
-    for (var c = i; c <= f; c = c + p) {
-        
+    var i = Number(document.getElementById("ini").value)
+    var f = Number(document.getElementById("fim").value)
+    var p = Number(document.getElementById("passo").value)
+    var final = document.getElementById("final")
+    var contagem = document.getElementById("contagem")
+    var contador = 0
+    var s = ""
+    var r = document.getElementById("res")
+    if (p = 0) {
+        p = 1
     }
-    console.log('FIM!')
+    if (i >= f) {
+        for (var c = i; c >= f; c -= p) {
+            s = s + c + " _ "    
+            contador += 1  
+        }
+    } else if (i <= f){
+        for (var c = i; c <= f; c += p) {
+            s = s + c + " _ "    
+            contador += 1  
+        }
+    }
+    r.innerHTML = `Resultado: ` 
+    contagem.innerHTML = s
+    final.innerHTML = `quantidade de numeros: ${contador}!` 
 }
 
 
