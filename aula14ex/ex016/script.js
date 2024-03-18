d905f611762bb067644d18a0491231337d2288ab
FelipeMaGomes <felipe.martins.gomes@outlook.com>
2024-03-18 11:27:18 -0300

    get element

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,11 +1,24 @@
 function contar() {
-    //var ini = getElementById('ini')
-    //var fim = getElementById('fim')
-    //var passo = getElementById('passo')
+    var i = document.getElementById("ini").value
+    var f = document.getElementById("fim").value 
+    var p = document.getElementById("passo").value
+    var r = document.getElementById("res") 
+    r.innerHTML = 'Contando: ';
+}
+
+
+
+
+
+
+/* function contar() {
+    var i = getElementById('ini')
+    var f = getElementById('fim')
+    var p = getElementById('passo')
    //var conta = getElementById('conta')
-    var res = getElementById('res')
-    res.innerHTML = 'Contando: '
-    for (var c = 1; c <= 8; c += 1) {
+    var r = getElementById('res') 
+    r.innerHTML = 'Contando: '
+    for (var c = i; c <= f; c += p) {
         console.log(`${c} `)
     }
-}
+} */
