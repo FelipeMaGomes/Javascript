a9f17b6d3f5b12209ac429714a3a5de9431db210
FelipeMaGomes <felipe.martins.gomes@outlook.com>
2024-03-29 13:07:21 -0300

    tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,5 +1,18 @@
-tabuada() {
-    let num = getElementById('num')
-    let res = getElementById('res')
-    
+function tabuada() {
+    var num = Number(document.getElementById("numero").value)
+    var seltab = document.getElementById("seltab")
+    var r = 0
+    if (document.getElementById("numero").value.length == 0 ) {
+        alert('Por favor, digite um número!')
+    } else {
+        seltab.options.length = 0
+        for (var c = 1; c <= 10; c += 1) {
+            let item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num*c}`
+            item.value = `tab${c}`
+            seltab.appendChild(item)
+           // r = num * c
+           // seltab.innerHTML += `<option value="">${num} x ${c} = ${r}</option>`
+        }
+    }
 }
