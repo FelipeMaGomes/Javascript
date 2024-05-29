9d1d06e4cfd475a7298a85358bb343536b2c80a4
FelipeMaGomes <felipe.martins.gomes@outlook.com>
2024-05-29 15:00:47 -0300

    array

--- aula16/analisador de números/script.js
+++ aula16/analisador de números/script.js
@@ -1,4 +1,11 @@
 function adicionar() {
     var v = Number(document.getElementById("valor").value)
-    
+    var o = document.getElementById("opcao")
+    var list = []
+    list.push(v)
+    o.innerHTML += `\n Valor ${v} adicionado.`
+}
+
+function finalizar() {
+
 }
